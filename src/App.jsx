f8b9862b591760141738d38d825dbdb74e6ed6d1
manish <patilmanish7204@gmail.com>
2023-10-10 20:24:58 +0530

    remove : unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [UserData, setUserData] = useState([])
-  
+  const [UserData, setUserData] = useState([]);
 
   return (
     <>
-
-   
       <div className="main">
         <div className="container">
           <div className="NavBar">
             <hi className="Fitness-Pro">Fitness Pro</hi>
-
-</div>
-          <input type="Date" className="name" placeholder='Enter Name Here' />
-          <br/>
-          <input type="text" className="weight"  placeholder='Enter Weight Here'/>
-          <br/>
-          <input type="text" className="height"  placeholder='Enter Height Here'/>
-          <br/>
-          <input type="text" className="Goal-height"  placeholder='Enter The Wight That You Have To Gain '/>
-          <br/>
-
-          <button className="submitBtn" type='submit'>Submit</button>
           </div>
+          <input type="Date" className="name" placeholder="Enter Name Here" />
+          <br />
+          <input
+            type="text"
+            className="weight"
+            placeholder="Enter Weight Here"
+          />
+          <br />
+          <input
+            type="text"
+            className="height"
+            placeholder="Enter Height Here"
+          />
+          <br />
+          <input
+            type="text"
+            className="Goal-height"
+            placeholder="Enter The Wight That You Have To Gain "
+          />
+          <br />
+
+          <button className="submitBtn" type="submit">
+            Submit
+          </button>
+        </div>
       </div>
-        
     </>
-  )
+  );
 }
 
-export default App
+export default App;
