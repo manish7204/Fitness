43dd5aeac4ff50ba0ea78c9ecac72db8336eccfd
manish <patilmanish7204@gmail.com>
2023-10-10 21:04:46 +0530

    Adding div to input tag

--- src/App.jsx
+++ src/App.jsx
@@ -11,41 +11,50 @@ function App() {
           <div className="NavBar">
             <hi className="Fitness-Pro">Fitness Pro</hi>
           </div>
+          <div className="Allinput">
+            <label>
+              Full Name:
+              <input
+                type="Text"
+                className="name"
+                placeholder="Enter Name Here"
+              />
+            </label>
 
-          <label>
-            Full Name:
-            <input type="Text" className="name" placeholder="Enter Name Here" />
-          </label>
+            <label>
+              Height:
+              <input
+                type="Text"
+                className="Height"
+                placeholder="Enter Height Here"
+              />
+            </label>
+            <br />
 
-          <label>
-            Height:
-            <input type="Text" className="Height" placeholder="Enter Height Here" />
-          </label>
-          <br />
+            <label>
+              Current Weight:
+              <input
+                type="text"
+                className="weight"
+                placeholder="Enter Weight Here"
+              />
+            </label>
+            <br />
 
-          <label>
-            Current Weight:
-            <input
-              type="text"
-              className="weight"
-              placeholder="Enter Weight Here"
-            />
-          </label>
-          <br />
+            <label>
+              Goal:
+              <input
+                type="text"
+                className="Goal"
+                placeholder="Enter Goal Here"
+              />
+            </label>
+            <br />
 
-          <label>
-            Goal:
-            <input
-              type="text"
-              className="Goal"
-              placeholder="Enter Goal Here"
-            />
-          </label>
-          <br />
-
-          <button className="submitBtn" type="submit">
-            Submit
-          </button>
+            <button className="submitBtn" type="submit">
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </>
