7219fabfd892a6dc5343b1cc74a50e9f87a00b11
manish <patilmanish7204@gmail.com>
2023-10-12 19:48:55 +0530

    add : login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import {Login} from "./pages/Login";
 
 function App() {
-  const [UserData, setUserData] = useState([]);
 
   return (
     <>
-      <div className="main">
-        <div className="container">
-          <div className="NavBar">
-            <hi className="Fitness-Pro">Fitness Pro</hi>
-          </div>
-          <div className="Allinput">
-            <label>
-              Full Name:
-              <input
-                type="Text"
-                className="name"
-                placeholder="Enter Name Here"
-              />
-            </label>
-
-            <label>
-              Height:
-              <input
-                type="Text"
-                className="Height"
-                placeholder="Enter Height Here"
-              />
-            </label>
-            <br />
-
-            <label>
-              Current Weight:
-              <input
-                type="text"
-                className="weight"
-                placeholder="Enter Weight Here"
-              />
-            </label>
-            <br />
-
-            <label>
-              Goal:
-              <input
-                type="text"
-                className="Goal"
-                placeholder="Enter Goal Here"
-              />
-            </label>
-            <br />
-
-            <button className="submitBtn" type="submit">
-              Submit
-            </button>
-          </div>
-        </div>
-      </div>
+      <Login/>
     </>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+
+export function Login() {
+
+  const[inputsVal,setInputVal]=useState({});
+
+  const allInputVal =() =>{
+
+  }
+  return (
+    <>
+      <div className="main">
+        <div className="container">
+          <div className="NavBar">
+            <h1 className="Fitness-Pro">Fitness Pro</h1>
+          </div>
+          <div className="Allinput">
+            <label className="label">
+              Full Name:
+              <input
+                type="Text"
+                className="name"
+                placeholder="Enter Name Here"
+              />
+            </label>
+
+            <label className="label">
+              Height:
+              <input
+                type="Text"
+                className="Height"
+                placeholder="Enter Height Here"
+              />
+            </label>
+            <br />
+
+            <label className="label">
+              Current Weight:
+              <input
+                type="text"
+                className="weight"
+                placeholder="Enter Weight Here"
+              />
+            </label>
+            <br />
+
+            <label className="label">
+              Goal:
+              <input
+                type="text"
+                className="Goal"
+                placeholder="Enter Goal Here"
+              />
+            </label>
+            <br />
+
+            <div className="DateInput">
+
+            <label className="label">
+              Duration
+              <br></br> From:
+              <input
+                type="Date"
+                className="FromDate"
+                placeholder="Enter Goal Here"
+              />
+            </label>
+
+            <label className="label">
+              To:
+              <input
+                type="Date"
+                className="ToDate"
+                placeholder="Enter Goal Here"
+              />
+            </label>
+
+            </div>
+            <button className="submitBtn" type="submit" onClick={allInputVal}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
